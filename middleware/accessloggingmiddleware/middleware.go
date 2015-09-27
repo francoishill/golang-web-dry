ebb09d149131b573e285d967476da22a01a3c423
Francois Hill (home win10) <francoishill11@gmail.com>
2015-09-27 12:28:55 +0200

    Added utils for: - http Request proxies - Access logging middleware

---
+++ middleware/accessloggingmiddleware/middleware.go
@@ -0,0 +1,41 @@
+package accessloggingmiddleware
+
+// Inspiration from:
+//  - https://github.com/codegangsta/negroni/blob/master/logger.go
+//  - Beego's context/input: https://github.com/astaxie/beego/blob/8f7246e17b504c858592a28a87e96fa7537a5aaf/context/input.go
+
+import (
+	"net/http"
+
+	"github.com/francoishill/golang-web-dry/requests/requestproxyutils"
+)
+
+type AccessInfo struct {
+	RemoteAddr string
+	RemoteIP   string
+	RequestURI string
+	Proxies    []string
+	UserAgent  string
+}
+
+type middleware struct {
+	withAccessInfo func(info *AccessInfo)
+}
+
+func (m *middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	if m.withAccessInfo != nil {
+		m.withAccessInfo(&AccessInfo{
+			RemoteAddr: r.RemoteAddr,
+			RemoteIP:   requestproxyutils.IP(r),
+			RequestURI: r.RequestURI,
+			Proxies:    requestproxyutils.Proxy(r),
+			UserAgent:  r.UserAgent(),
+		})
+	}
+
+	next(w, r)
+}
+
+func NewAccessLoggingMiddleware(withAccessInfo func(info *AccessInfo)) *middleware {
+	return &middleware{withAccessInfo}
+}
---
+++ requests/requestproxyutils/requestproxyutils.go
@@ -0,0 +1,28 @@
+package requestproxyutils
+
+import (
+	"net/http"
+	"strings"
+)
+
+func Proxy(r *http.Request) []string {
+	if ips := r.Header.Get("X-Forwarded-For"); ips != "" {
+		return strings.Split(ips, ",")
+	}
+	return []string{}
+}
+
+func IP(r *http.Request) string {
+	ips := Proxy(r)
+	if len(ips) > 0 && ips[0] != "" {
+		rip := strings.Split(ips[0], ":")
+		return rip[0]
+	}
+	ip := strings.Split(r.RemoteAddr, ":")
+	if len(ip) > 0 {
+		if ip[0] != "[" {
+			return ip[0]
+		}
+	}
+	return "127.0.0.1"
+}
