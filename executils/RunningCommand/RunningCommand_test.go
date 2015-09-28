973b2f5b751d58eaf7a1bb5efb0e03f22662c536
Francois Hill (home win10) <francoishill11@gmail.com>
2015-09-28 18:16:59 +0200

    We are testing with window's `cmd` executable. Added a `switch` on the `runtime.GOOS` and a `TODO` to implement non-windows tests.

--- executils/RunningCommand/RunningCommand_test.go
+++ executils/RunningCommand/RunningCommand_test.go
@@ -2,55 +2,61 @@ package RunningCommand
 
 import (
 	. "github.com/smartystreets/goconvey/convey"
+	"runtime"
 	"strings"
 	"testing"
 )
 
 func TestStartAndDoWithFeedback(t *testing.T) {
 	Convey("Testing StartAndDoWithFeedback", t, func() {
-		Convey("Start application to run echo with no errors", func() {
-			for i := 0; i < 20; i++ {
-				rc := New(nil, nil, "cmd", "/c", "echo", "hello")
+		//TODO: Add tests for non-Windows
+		switch runtime.GOOS {
+		case "windows":
+			Convey("Start application to run echo with no errors", func() {
+				for i := 0; i < 20; i++ {
+					rc := New(nil, nil, "cmd", "/c", "echo", "hello")
 
-				outLines := []string{}
-				errLines := []string{}
-				rc.StartAndDoWithFeedback(0, func(isErr bool, fb string) {
-					if isErr {
-						errLines = append(errLines, fb)
-					} else {
-						outLines = append(outLines, fb)
-					}
-				})
+					outLines := []string{}
+					errLines := []string{}
+					rc.StartAndDoWithFeedback(0, func(isErr bool, fb string) {
+						if isErr {
+							errLines = append(errLines, fb)
+						} else {
+							outLines = append(outLines, fb)
+						}
+					})
 
-				So(len(outLines), ShouldEqual, 2)
-				So(strings.TrimSpace(outLines[0]), ShouldEqual, "Command has started.")
-				So(strings.TrimSpace(outLines[1]), ShouldEqual, "hello")
+					So(len(outLines), ShouldEqual, 2)
+					So(strings.TrimSpace(outLines[0]), ShouldEqual, "Command has started.")
+					So(strings.TrimSpace(outLines[1]), ShouldEqual, "hello")
 
-				So(len(errLines), ShouldEqual, 0)
-			}
-		})
+					So(len(errLines), ShouldEqual, 0)
+				}
+			})
 
-		Convey("Start application WITH errors", func() {
-			for i := 0; i < 20; i++ {
-				rc := New(nil, nil, "cmd", "/c", "echo", "hello", "&", "exit", "/b", "13")
+			Convey("Start application WITH errors", func() {
+				for i := 0; i < 20; i++ {
+					rc := New(nil, nil, "cmd", "/c", "echo", "hello", "&", "exit", "/b", "13")
 
-				outLines := []string{}
-				errLines := []string{}
-				rc.StartAndDoWithFeedback(0, func(isErr bool, fb string) {
-					if isErr {
-						errLines = append(errLines, fb)
-					} else {
-						outLines = append(outLines, fb)
-					}
-				})
+					outLines := []string{}
+					errLines := []string{}
+					rc.StartAndDoWithFeedback(0, func(isErr bool, fb string) {
+						if isErr {
+							errLines = append(errLines, fb)
+						} else {
+							outLines = append(outLines, fb)
+						}
+					})
 
-				So(len(outLines), ShouldEqual, 2)
-				So(strings.TrimSpace(outLines[0]), ShouldEqual, "Command has started.")
-				So(strings.TrimSpace(outLines[1]), ShouldEqual, "hello")
+					So(len(outLines), ShouldEqual, 2)
+					So(strings.TrimSpace(outLines[0]), ShouldEqual, "Command has started.")
+					So(strings.TrimSpace(outLines[1]), ShouldEqual, "hello")
 
-				So(len(errLines), ShouldEqual, 1)
-				So(strings.TrimSpace(errLines[0]), ShouldEqual, "Unable to finish waiting for command: exit status 13")
-			}
-		})
+					So(len(errLines), ShouldEqual, 1)
+					So(strings.TrimSpace(errLines[0]), ShouldEqual, "Unable to finish waiting for command: exit status 13")
+				}
+			})
+			break
+		}
 	})
 }
