96b0edfec51e03d24cd78c8d661d1401b04cd686
Francois Hill (fhill-wks) <francoishill11@gmail.com>
2016-02-26 16:12:44 +0200

    Create file parent dir if not exist.

--- zip/ziputils/SaveReaderToFile.go
+++ zip/ziputils/SaveReaderToFile.go
@@ -4,9 +4,19 @@ import (
 	. "github.com/francoishill/golang-web-dry/errors/checkerror"
 	"io"
 	"os"
+	"path/filepath"
+
+	"github.com/francoishill/golang-web-dry/osutils"
 )
 
 func SaveReaderToFile(logger SimpleLogger, bodyReader io.Reader, saveFilePath string) {
+	fullDestinationDirPath := filepath.Dir(saveFilePath)
+	if !osutils.DirectoryExists(fullDestinationDirPath) {
+		logger.Debug("(TAR) Creating directory '%s' ( parent of file '%s')", fullDestinationDirPath, filepath.Base(saveFilePath))
+		err := os.MkdirAll(fullDestinationDirPath, os.FileMode(0655))
+		CheckError(err)
+	}
+
 	out, err := os.Create(saveFilePath)
 	CheckError(err)
 	defer out.Close()
